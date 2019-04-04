2d205bf417022b77c6922724d036dea6098f4dc3
kludDesign <vincentgillibert@gmail.com>
2019-04-04 16:38:34 +0200

    Modif french to english variable

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,19 +1,19 @@
 def signup
-  puts "entrer mot de passe"
+  puts "Entre ton mot de passe"
   print ">"
-  mot_de_passe = gets.chomp
-  return mot_de_passe
+  password = gets.chomp
+  return password
 end
 
 def login
   puts "Met ton mot de passe pour accéder à la page :"
   print ">"
-  mot_de_passe_2 = gets.chomp
-  return mot_de_passe_2
+  password_2 = gets.chomp
+  return password_2
 end
 
-def compare(mot_de_passe, mot_de_passe_2)
-  if mot_de_passe == mot_de_passe_2
+def welcome_screen(password, password_2)
+  if password == password_2
     puts "Tu es sur le site de la NASA"
   else
     puts "Mauvais MDP"
@@ -21,9 +21,9 @@ def compare(mot_de_passe, mot_de_passe_2)
 end
 
 def perform
-  mot_de_passe = signup
-  mot_de_passe_2 = login
-  compare(mot_de_passe, mot_de_passe)
+  password = signup
+  password_2 = login
+  welcome_screen(password, password_2)
 end
 
 perform
