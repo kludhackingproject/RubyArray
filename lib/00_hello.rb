dd39a5872943fafb8f3933299902523632b7d8cc
kludDesign <vincentgillibert@gmail.com>
2019-04-04 13:44:42 +0200

    Push for the 1st exercice

---
+++ lib/00_hello.rb
@@ -0,0 +1,38 @@
+############ Partie 1 ###############
+
+# def say_hello
+#   first_name = "Ilhem"
+#   puts "Bonjour #{first_name}"
+# end
+# say_hello
+
+############ Partie 2 ###############
+
+# def ask_first_name
+#   puts "Quel est ton prénom?"
+#   print "> "
+#   first_name = gets.chomp
+#   puts "Bonjour #{first_name}"
+# end
+#
+# ask_first_name
+
+############ Partie 3 ###############
+
+def ask_first_name
+  puts "Quel est ton prénom?"
+  print "> "
+  first_name = gets.chomp
+  return first_name
+end
+
+def say_hello(first_name)
+  puts "Bonjour #{first_name}"
+end
+
+def perform
+  first_name = ask_first_name
+  say_hello(first_name)
+end
+
+perform
